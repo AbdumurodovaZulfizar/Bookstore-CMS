9cdcd87429847496f934a088022f2fb2fe3b7924
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-09-30 08:50:17 +0500

    Create App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = () => {
+  return (
+    <div>
+      <BooksForm />
+      <BooksList />
+    </div>
+  );
+}
+
+export default App;
