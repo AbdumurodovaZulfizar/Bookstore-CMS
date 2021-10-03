c9bae6f78b5158a790d9dbef96b001c0016961d1
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-10-03 17:40:58 +0500

    Modigy filter reducer

--- src/reducres/filter.js
+++ src/reducres/filter.js
@@ -1,7 +1,11 @@
 const defaultState = 'All';
 
 const filterReducer = (state = defaultState, action) => {
-
+  if(action.type === 'CHANGE_FILTER') {
+    return action.value
+  } else {
+    return state
+  }
 }
 
 export default filterReducer;
