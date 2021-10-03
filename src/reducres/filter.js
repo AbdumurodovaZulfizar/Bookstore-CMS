a51d8f45f70977ca459de73a971283e60b6bd454
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-10-03 19:50:49 +0500

    Fix linter errors

--- src/reducres/filter.js
+++ src/reducres/filter.js
@@ -3,8 +3,8 @@ const defaultState = 'All';
 const filterReducer = (state = defaultState, action) => {
   if (action.type === 'CHANGE_FILTER') {
     return action.value;
-  };
+  }
   return state;
-}
+};
 
 export default filterReducer;
