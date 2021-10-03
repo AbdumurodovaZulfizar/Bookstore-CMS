2190c5997a481ebb54dd1cedbf33d31895fcd0a4
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-10-03 18:21:03 +0500

    Combine reducers + Add filter to booklist

--- src/components/FilterCategory.js
+++ src/components/FilterCategory.js
@@ -4,11 +4,12 @@ import PropTypes from 'prop-types';
 const categories = ["All", "Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"];
 
 const FilterCategory = ({filter, handleFilter}) => (
-  <div>
+  <div className="row my-3">
     <select
       name="category"
       value={filter}
       onChange={(e) => handleFilter(e.target.value)}
+      className='col-8 mx-auto py-1'
     >
       {categories.map((item) => (
         <option value={item} key={item}>
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,51 +1,68 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { handleRemoveBook } from '../actions/index';
+import { handleRemoveBook, changeFilter } from '../actions/index';
 import BookConatiner from '../components/Book';
+import FilterCategory from '../components/FilterCategory';
 
-const BooksList = ({ books, handleRemoveBook }) => (
-  <table className="table col-9 mx-auto">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Title</th>
-        <th scope="col">Category</th>
-        <th scope="col">Buttons</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-      books.map((book) => (
-        <BookConatiner
-          id={book.id}
-          title={book.title}
-          category={book.category}
-          key={book.id}
-          handleRemoveBook={handleRemoveBook}
-        />
-      ))
-      }
-    </tbody>
-  </table>
+const BooksList = ({ books, handleRemoveBook, filter, changeFilter }) => (
+  <div>
+    <FilterCategory filter={filter} handleFilter={changeFilter} />
+    <table className="table col-9 mx-auto">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Category</th>
+          <th scope="col">Buttons</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+        books.map((book) => (
+          <BookConatiner
+            id={book.id}
+            title={book.title}
+            category={book.category}
+            key={book.id}
+            handleRemoveBook={handleRemoveBook}
+          />
+        ))
+        }
+      </tbody>
+    </table>
+  </div>
 );
 
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.instanceOf(Object)),
   handleRemoveBook: PropTypes.func,
+  filter: PropTypes.string,
+  changeFilter: PropTypes.func,
 };
 
 BooksList.defaultProps = {
   books: [],
   handleRemoveBook: null,
+  filter: 'All',
+  changeFilter: null,
+};
+
+const getBooks = (books, filter) => {
+  if (filter === 'All') {
+    return books;
+  }
+  return books.filter((book) => book.category === filter);
 };
 
 const mapStateToProps = (state) => ({
-  books: state.books,
+  books: getBooks(state.books, state.filter),
+  filter: state.filter,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   handleRemoveBook: (id) => dispatch(handleRemoveBook(id)),
+  changeFilter: (category) => dispatch(changeFilter(category)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
